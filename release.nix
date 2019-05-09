b06e2cdfa32c22f02deb5e977aa25a3f4acb381c
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-09 15:49:53 +0800

    Update release.nix

--- release.nix
+++ release.nix
@@ -1,5 +1,19 @@
 let
-  pkgs = import <nixpkgs> {};
+  # generic function to fetch a tarball from a given .json file
+  # like those provided in ./pins
+  fetchTarballFromJson = jsonFile: with builtins;
+    let spec = fromJSON (readFile jsonFile); in fetchTarball {
+      inherit (spec) sha256; url = "${spec.url}/archive/${spec.rev}.tar.gz"; }
+  
+  # Allow overriding the fetch with a name on the command line so we can use -I
+  fetchTarballFromJsonWithOverride = override: srcJson: with builtins;
+    let try = tryEval (findFile nixPath override); in
+    if try.success then trace "using search host <${override}>" try.value
+    else fetchTarballFromJson srcJson;
+
+  # fetch nixpkgs. iohk hydra doesn't provide <nixpkgs>, so we'll have to use
+  # a pinned one.
+  pkgs = fetchTarballFromJsonWithOverride "nixpkgs" ./pins/nixpkgs-src.json;
 
   # jobs contain a key -> value mapping that tells hydra which
   # derivations to build.  There are some predefined helpers in 
