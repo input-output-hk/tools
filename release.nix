315e05e7bea5bb2aa284b322414e567b96fe6baf
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-09 15:34:13 +0800

    Update release.nix

--- release.nix
+++ release.nix
@@ -1,5 +1,21 @@
 let
   pkgs = import <nixpkgs> {};
-  jobs = rec { };
+
+  # jobs contain a key -> value mapping that tells hydra which
+  # derivations to build.  There are some predefined helpers in 
+  # https://github.com/NixOS/nixpkgs/tree/master/pkgs/build-support/release
+  # which can be accessed via `pkgs.releaseTools`.
+  #
+  # It is however not necessary to use those.
+  #
+  jobs = rec {
+    # a very simple job. All it does is call a shell script that print Hello World.
+    hello-world = derivation {
+      name = "Hello World";
+      builder = pkgs.writeShellScriptBin "helloWorld" "echo Hello World";
+      system = builtins.currentSystem;
+    };
+    
+  };
 in
   jobs
