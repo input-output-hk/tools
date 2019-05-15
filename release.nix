0a0cc57917c761b90d865afe10dd21659d25c61a
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-15 15:16:06 +0800

    Add more crompilers (macOS, windows)

--- release.nix
+++ release.nix
@@ -5,8 +5,22 @@ let
   # a pinned one.
   pkgs = importPinned "nixpkgs" {};
 
+  # cross system settings
+  mingwW64 = pkgs.lib.systems.examples.mingwW64;
+
   # import iohk-nix with the same pin as the nixpkgs above.
-  iohk-nix = importPinned "iohk-nix" { nixpkgsJsonOverride = ./pins/nixpkgs-src.json; };
+
+  # linux packages
+  x86_64-linux = importPinned "iohk-nix"
+    { nixpkgsJsonOverride = ./pins/nixpkgs-src.json; system = "x86_64-linux"; };
+
+  # macos packages
+  x86_64-macos = importPinned "iohk-nix"
+    { nixpkgsJsonOverride = ./pins/nixpkgs-src.json; system = "x86_64-darwin"; };
+
+  # windows cross compiled on linux
+  x86_64-mingw32 = importPinned "iohk-nix"
+    { nixpkgsJsonOverride = ./pins/nixpkgs-src.json; system = "x86_64-linux"; crossSystem = mingwW64; };
 
   # jobs contain a key -> value mapping that tells hydra which
   # derivations to build.  There are some predefined helpers in
@@ -22,7 +36,16 @@ let
     # this should give us our patched compiler. (e.g. the one
     # from the pinned nixpkgs set with all the iohk-nix
     # patches applied.
-    ghc864 = iohk-nix.pkgs.haskell.compiler.ghc864;
+
+    # linux
+    ghc864.x86_64-linux = x86_64-linux.pkgs.haskell.compiler.ghc864;
+
+    # macOS
+    ghc864.x86_64-macos = x86_64-macos.pkgs.haskell.compiler.ghc864;
+
+    # linux -> win32
+    "${mingwW64.config}-ghc864".x86_64-linux = x86_64-mingw32.pkgs.haskell.compiler.ghc864;
+
   };
 in
   jobs
