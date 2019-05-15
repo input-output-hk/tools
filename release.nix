2f43a117fbd24ea0bf4402a87d47e0b153353cc7
Moritz Angermann <moritz.angermann@gmail.com>
2019-05-15 14:15:11 +0800

    Add ghc864. Let's see if we can build it.

--- release.nix
+++ release.nix
@@ -5,6 +5,9 @@ let
   # a pinned one.
   pkgs = importPinned "nixpkgs" {};
 
+  # import iohk-nix with the same pin as the nixpkgs above.
+  iohk-nix = importPinned "iohk-nix" { nixpkgsJsonOverride = ./pins/nixpkgs-src.json; };
+
   # jobs contain a key -> value mapping that tells hydra which
   # derivations to build.  There are some predefined helpers in
   # https://github.com/NixOS/nixpkgs/tree/master/pkgs/build-support/release
@@ -15,6 +18,11 @@ let
   jobs = rec {
     # a very simple job. All it does is call a shell script that print Hello World.
     hello-world = import ./jobs/trivial-hello-world { inherit pkgs; };
+
+    # this should give us our patched compiler. (e.g. the one
+    # from the pinned nixpkgs set with all the iohk-nix
+    # patches applied.
+    ghc864 = iohk-nix.pkgs.haskell.compiler.ghc864;
   };
 in
   jobs
