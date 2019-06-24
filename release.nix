a3762667c28ea2419c47f41802ed6d88a7cf22e0
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-06-24 21:10:46 +1200

    Try macos plan-nix

--- release.nix
+++ release.nix
@@ -46,6 +46,7 @@ let
     hello-world = import ./jobs/trivial-hello-world { inherit pkgs; };
 
     asterius-plan-nix = asterius.plan-nix;
+    asterius-plan-nix-macos = asterius-macos.plan-nix;
     
     asterius-boot = asterius.nix-tools._raw.asterius-boot;
     asterius-nix-tools = asterius.nix-tools._raw.haskell.nix-tools;
@@ -57,7 +58,6 @@ let
     asterius-nix = asterius.nix-tools._raw.pkgs.nix;
     asterius-boehmgc = asterius.nix-tools._raw.pkgs.boehmgc;
     asterius-test = asterius.nix-tools.tests.asterius;
-    asterius-plan-nix-macos = asterius-macos.nix-tools._raw.plan-nix;
     # asterius-test-macos = asterius-macos.nix-tools.tests.asterius;
 
     # this should give us our patched compiler. (e.g. the one
