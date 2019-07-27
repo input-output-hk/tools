35f4200d09a0949261be22c3951127850171f55b
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-07-28 03:07:41 +1200

    Focus on jsaddle-dom

--- release.nix
+++ release.nix
@@ -56,9 +56,9 @@ let
     leksah-shells = leksah.shells;
 
     leksah-plan-nix-macos = leksah-macos.plan-nix;
-    wrapped-leksah-macos = leksah-macos.nix-tools._raw.wrapped-leksah;
+#    wrapped-leksah-macos = leksah-macos.nix-tools._raw.wrapped-leksah;
     jsaddle-dom-macos = leksah-macos.nix-tools.libs.jsaddle-dom;
-    leksah-shells-macos = leksah-macos.shells;
+#    leksah-shells-macos = leksah-macos.shells;
 
     leksah-plan-nix-mingw32 = leksah-mingw32.plan-nix;
 
