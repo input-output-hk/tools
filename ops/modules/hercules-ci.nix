87a9125cf45c50e7fd17945178b80ec92a10be33
Moritz Angermann <moritz.angermann@gmail.com>
2019-10-25 09:23:26 +0800

    bump hercules agent

--- ops/modules/hercules-ci.nix
+++ ops/modules/hercules-ci.nix
@@ -7,7 +7,9 @@ let
 
   # take latest master branch (not stable branch)
 #  hercules-ci-agent = builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/05da1213155e84c4daea58076692b02d30c1d587.tar.gz";
-  hercules-ci-agent = builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/hercules-ci-agent-0.5.0.tar.gz";
+  # this is #162 fix ifd caching
+  hercules-ci-agent = builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/8fcd34913ffa9577ac5e54a9b7a5a89e5ad97b0b.tar.gz";
+#  hercules-ci-agent = builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/hercules-ci-agent-0.5.0.tar.gz";
 in
 
 { config, pkgs, ... }:
