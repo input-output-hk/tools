04cfb31f71333b15ff6d000499d56466e214f719
Moritz Angermann <moritz.angermann@gmail.com>
2019-12-09 16:17:27 +0800

    bump nixos version

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -4,7 +4,7 @@
 top="$(cd `dirname $0`; pwd)"
 
 # export NIX_PATH="$top"
-export NIX_PATH="nixpkgs=http://nixos.org/channels/nixos-19.03/nixexprs.tar.xz"
+export NIX_PATH="nixpkgs=http://nixos.org/channels/nixos-19.09/nixexprs.tar.xz"
 export NIXOPS_STATE="$top/secrets/state.nixops"
 export NIXOPS_DEPLOYMENT=hercules-ci-agents
 
