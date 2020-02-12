4e4dfe5252fb166083b5050e38b137b3001181dc
Moritz Angermann <moritz.angermann@gmail.com>
2020-02-12 10:32:42 +0800

    add iohk binary cache; disable hercules for iohk

--- ops/modules/iohk-binary-cache.nix
+++ ops/modules/iohk-binary-cache.nix
@@ -5,7 +5,7 @@
 
 {
   nix = {
-    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-    binaryCaches = [ "https://hydra.iohk.io" ];
+    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
+    binaryCaches = [ "https://hydra.iohk.io" "iohk.cachix.org" ];
   };
 }
