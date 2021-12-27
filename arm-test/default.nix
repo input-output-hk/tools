f27aba58db24b8c51405c0f63e3a46f5ea2b4be8
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-27 07:33:19 +0000

    Yuck!

--- arm-test/default.nix
+++ arm-test/default.nix
@@ -17,9 +17,10 @@ in
     nixpkgsArgs.overlays ++
     [
       (final: prev: { libsodium = final.callPackage ./libsodium.nix {}; })
-      (final: prev: { llvmPackages_13.compiler-rt-libc = prev.llvmPackages_13.compiler-rt-libc.overrideAttrs (old: {
-        cmakeFlags = with old.stdenv.hostPlatform; old.cmakeFlags ++ (if isMusl && isAarch64 then [ "-DCOMPILER_RT_BUILD_MEMPROF=OFF" ] else []);
-      });})
+      (final: prev: { llvmPackages_13 = prev.llvmPackages_13 // {
+          compiler-rt-libc = prev.llvmPackages_13.compiler-rt-libc.overrideAttrs (old: {
+            cmakeFlags = with old.stdenv.hostPlatform; old.cmakeFlags ++ (if isMusl && isAarch64 then [ "-DCOMPILER_RT_BUILD_MEMPROF=OFF" ] else []);
+          });}; })
     ]
     ;
     inherit system;
