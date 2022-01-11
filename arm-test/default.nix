df469cdf4752d9352777e8bccd9d812d8de48b7a
Moritz Angermann <moritz.angermann@gmail.com>
2022-01-11 09:32:50 +0000

    bump

--- arm-test/default.nix
+++ arm-test/default.nix
@@ -167,7 +167,7 @@ nativePkgs.lib.mapAttrs (_: pkgs: rec {
             packages.iohk-monitoring.patches = [ ./cardano-node-patches/iohk-monitoring-framework-625.diff ];
             # android default inlining threshold seems to be too high for closure_sizeW to be inlined properly.
             packages.cardano-prelude.ghcOptions = [ "-optc=-mllvm" "-optc-inlinehint-threshold=500" ];
-            packages.cardano-node.ghcOptions = [ "-optc=-fPIE" "-fPIE" ];
+            packages.cardano-node.ghcOptions = [ "-optc=-fPIE" "-fPIE" "-Wl,-fPIE" "-Wl,-pie"] ;
           })
           ({ pkgs, lib, ... }: lib.mkIf (!pkgs.stdenv.hostPlatform.isGhcjs) {
             packages = {
